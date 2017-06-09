1376dec1b23a598ec871a0f9ec7ce4bb716d5a62
kylo_ren <kylo_ren@localhost>
2017-06-09 15:41:41 +0200

    Commit 1

---
+++ examen_recuperacion/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="procesar.php" method="GET">
+            <label>Login</label>
+            <input type="text" name="login"></input>
+             <label>Password</label>
+             <input type="password" name="password"></input>
+             <button type="submit">Enviar</button>
+        </form>
+    </body>
+</html>
